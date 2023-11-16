b2886f9a7c57a70b884bd2be1164e4df79998757
Zain Ahmed <zain@zaintech.net>
2023-11-16 09:48:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,38 @@ import time
 import win32api
 import requests
 from datetime import datetime, timedelta
-serversidekey = "oAayX9wXdv7RDs425pXXc8zTa9TqHqWKOQcs0qZKmFGwpomqmL87rLSSu81By2gZ"
+serversidekey = "REDACTED"
 dak = 0
 pdat = datetime.now()
+lastPostTime = datetime.now() - timedelta(minutes=30)
+prevstatus = "active"
 while True:
     dat = datetime.now()
     mak = win32api.GetLastInputInfo()
-    print(mak)
-    print(dak)
-    print(dat)
-    print(pdat)
-
-    print(dat-pdat)
-    print(timedelta(seconds=5))
+    print(f"MASTER: ||||| {prevstatus} ||||||")
 #    if dak is not mak:
-    print(f"{dak} {mak}")
     if dak == mak:
-        print("yeah")
-        if dat - pdat >  timedelta(seconds=5): print("idle")
-#        requests.post("https://2.hy.pr/activity/mon", json={"lastActivity":dat.strftime(),"status":"idle"} headers={'Auth':serversidekey})
+        if dat - pdat >  timedelta(minutes=10):
+            print("idle")
+            if dat - lastPostTime > timedelta(minutes=20) or prevstatus == "active":
+                stri = "POST IDLE"
+                print(f"\033[95m{stri}\033[0m")
+                requests.post("https://2.hy.pr/activity/mon", json={"lastActivity":dat.strftime("%Y-%m-%d %H:%M:%S"),"status":"idle"}, headers={'Auth':serversidekey})
+                lastPostTime = datetime.now()
+            prevstatus = "idle"
+            time.sleep(1)
+            continue
         else: print("active1")
     else:
         dak = mak
         pdat=dat
+
         print("active2")
 
-
+        if dat - lastPostTime > timedelta(minutes=10) or prevstatus == "idle":
+            stri = "POST ACTIVE"
+            print(f"\033[95m{stri}\033[0m")
+            requests.post("https://2.hy.pr/activity/mon", json={"lastActivity":dat.strftime("%Y-%m-%d %H:%M:%S"),"status":"active"}, headers={'Auth':serversidekey})
+            lastPostTime = datetime.now()
+        prevstatus = "active"
     time.sleep(1)
